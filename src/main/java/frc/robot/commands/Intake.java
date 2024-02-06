9980013c746da1bf961e7473bc370b3137e8e404
Mateo Johnson <johnson.12@themetroschool.org>
2024-02-06 12:02:45 -0500

    added color recognition

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -8,6 +8,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatchResult;
@@ -16,10 +17,14 @@ import com.revrobotics.ColorMatch;
 
 public class Intake extends CommandBase {
   /** Creates a new silly. */
-  public static final I2C.Port I2CPort = I2C.Port.kOnboard;
-  public static final ColorSensorV3 colorSensor = new ColorSensorV3(I2CPort);
-  private final ColorMatch colorMatcher = new ColorMatch(); //CREATES A NEW COLOR MATCHER (USED TO REGISTER AND DETECT KNOWN COLORS)
-  private final Color orange = new Color(0.25, 0.73, 0.48); //CREATES A COLOR
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final Color kBlueTarget = new Color(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = new Color(0.197, 0.561, 0.240);
+  private final Color kRedTarget = new Color(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = new Color(0.361, 0.524, 0.113);
+  private final Color kOrangeTarget = new Color(0.453, 0.405, 0.141); //CREATES A COLOR
   public Intake() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -31,27 +36,45 @@ public class Intake extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    colorMatcher.addColorMatch(orange); //ADD A COLOR FOR REFERENCE TO THE COLOR MATCHER
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget); 
+    m_colorMatcher.addColorMatch(kOrangeTarget);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Color detectedColor = colorSensor.getColor(); //DETECT THE COLOR FROM THE COLOR SENSOR
 
-    colorMatcher.setConfidenceThreshold(0.5);
-    ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
-    colorSensor.getRawColor();
-    colorMatcher.matchClosestColor(detectedColor);
+    Color detectedColor = m_colorSensor.getColor();
+    String colorString;
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
 
-    if (match.color != orange) {
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
       intake1.set(0.6);
       intake2.set(0.6);
-    } else if (match.color == orange) {
+    } else if (match.color == kOrangeTarget){
+      colorString = "Orange";
       intake1.set(0);
       intake2.set(0);
+    } else {
+      colorString = "Unknown";
+      intake1.set(0.6);
+      intake2.set(0.6);
     }
-
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
 
   }
 
