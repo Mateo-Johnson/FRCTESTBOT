e980b9af9a86855d28c8c4c60e2fba8f79210a7a
Mateo Johnson <johnson.12@themetroschool.org>
2024-02-06 15:09:40 -0500

    silly

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -18,13 +18,15 @@ import com.revrobotics.ColorMatch;
 public class Intake extends CommandBase {
   /** Creates a new silly. */
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-  private final ColorMatch m_colorMatcher = new ColorMatch();
-  private final Color kBlueTarget = new Color(0.143, 0.427, 0.429);
-  private final Color kGreenTarget = new Color(0.197, 0.561, 0.240);
-  private final Color kRedTarget = new Color(0.561, 0.232, 0.114);
-  private final Color kYellowTarget = new Color(0.361, 0.524, 0.113);
-  private final Color kOrangeTarget = new Color(0.453, 0.405, 0.141); //CREATES A COLOR
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch colorMatcher = new ColorMatch();
+  private final Color BlueTarget = new Color(0.143, 0.427, 0.429); //CREATES A COLOR
+  private final Color GreenTarget = new Color(0.197, 0.561, 0.240); //CREATES A COLOR
+  private final Color RedTarget = new Color(0.561, 0.232, 0.114); //CREATES A COLOR
+  private final Color YellowTarget = new Color(0.361, 0.524, 0.113); //CREATES A COLOR
+  private final Color OrangeTarget = new Color(0.453, 0.405, 0.141); //CREATES A COLOR
+  public static boolean hasRing;
+
   public Intake() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -36,42 +38,51 @@ public class Intake extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_colorMatcher.addColorMatch(kBlueTarget);
-    m_colorMatcher.addColorMatch(kGreenTarget);
-    m_colorMatcher.addColorMatch(kRedTarget);
-    m_colorMatcher.addColorMatch(kYellowTarget); 
-    m_colorMatcher.addColorMatch(kOrangeTarget);
+    colorMatcher.addColorMatch(BlueTarget);
+    colorMatcher.addColorMatch(GreenTarget);
+    colorMatcher.addColorMatch(RedTarget);
+    colorMatcher.addColorMatch(YellowTarget); 
+    colorMatcher.addColorMatch(OrangeTarget);
+    hasRing = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
 
-    Color detectedColor = m_colorSensor.getColor();
+    Color detectedColor = colorSensor.getColor();
     String colorString;
-    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
 
-    if (match.color == kBlueTarget) {
+    if (match.color == BlueTarget) { //IF THE THING
       colorString = "Blue";
-    } else if (match.color == kRedTarget) {
+      ringInIntake(false); //FLAG TO MARK WHEN THERE ISN'T A RING IN INTAKE
+
+    } else if (match.color == RedTarget) {
       colorString = "Red";
-      intake1.set(0);
-      intake2.set(0);
-    } else if (match.color == kGreenTarget) {
+      stopIntake();
+      ringInIntake(true); //FLAG TO MARK WHEN THERE IS A RING IN INTAKE - 
+
+    } else if (match.color == GreenTarget) {
       colorString = "Green";
-    } else if (match.color == kYellowTarget) {
+      ringInIntake(false); //FLAG TO MARK WHEN THERE ISN'T A RING IN INTAKE
+
+    } else if (match.color == YellowTarget) {
       colorString = "Yellow";
-      intake1.set(0.6);
-      intake2.set(0.6);
-    } else if (match.color == kOrangeTarget){
+      runIntake(0.6);
+      ringInIntake(false); //FLAG TO MARK WHEN THERE ISN'T A RING IN INTAKE 
+
+    } else if (match.color == OrangeTarget){
       colorString = "Orange";
-      intake1.set(0);
-      intake2.set(0);
+      stopIntake();
+      ringInIntake(true); //FLAG TO MARK WHEN THERE IS A RING IN INTAKE - 
+
     } else {
       colorString = "Unknown";
-      intake1.set(0.6);
-      intake2.set(0.6);
+      runIntake(0.6);
+      ringInIntake(false); //FLAG TO MARK WHEN THERE ISN'T A RING IN INTAKE
     }
+
     SmartDashboard.putNumber("Red", detectedColor.red);
     SmartDashboard.putNumber("Green", detectedColor.green);
     SmartDashboard.putNumber("Blue", detectedColor.blue);
@@ -83,9 +94,25 @@ public class Intake extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    stopIntake();
+    hasRing = false;
+  }
+
+
+
+  //FUNCTIONS FOR SIMPLICITY
+  public void runIntake(double speed) {
+    intake1.set(speed);
+    intake2.set(speed);
+  }
+
+  public void stopIntake(){
     intake1.set(0);
     intake2.set(0);
+  }
 
+  public void ringInIntake(boolean trueslashfalse) {
+    hasRing = trueslashfalse;
   }
 
   // Returns true when the command should end.
@@ -94,3 +121,5 @@ public class Intake extends CommandBase {
     return false;
   }
 }
+
+
--- src/main/java/frc/robot/commands/Outtake.java
+++ src/main/java/frc/robot/commands/Outtake.java
@@ -6,8 +6,9 @@ package frc.robot.commands;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.commands.Intake;
 
 public class Outtake extends CommandBase {
   /** Creates a new silly. */
@@ -15,26 +16,54 @@ public class Outtake extends CommandBase {
     // Use addRequirements() here to declare subsystem dependencies.
   }
   //OUTTAKE
-  public static final CANSparkMax outtake1 = new CANSparkMax(14, MotorType.kBrushless);
-  public static final CANSparkMax outtake2 = new CANSparkMax(11, MotorType.kBrushless);
+  public static final CANSparkMax OUT1 = new CANSparkMax(14, MotorType.kBrushless);
+  public static final CANSparkMax OUT2 = new CANSparkMax(11, MotorType.kBrushless);
+  public static final CANSparkMax IN1 = Intake.intake1;
+  public static final CANSparkMax IN2 = Intake.intake2;
+
+
+  double rpm1 = OUT1.getEncoder().getVelocity();
+  double rpm2 = OUT2.getEncoder().getVelocity();
+  double avgRPM = (rpm1 + rpm2)/2;
+  double rpm3 = IN1.getEncoder().getVelocity();
+  double rpm4 = IN2.getEncoder().getVelocity();
 
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    outtake1.set(1);
-    outtake2.set(1);
+
+    if (Intake.hasRing == true){
+      
+    if (avgRPM <= 5000) { //IF THE MOTORS ARE SPINNING FAST ENOUGH 
+      OUT1.set(1); //SET UP OUTTAKE MOTOR 1 FOR SHOOTING
+      OUT2.set(1); //SET UP OUTTAKE MOTOR 2 FOR SHOOTING
+      IN1.set(0.6); //USE INTAKE MOTOR 1 TO FEED INTO OUTTAKE
+      IN2.set(0.6); //USE INTAKE MOTOR 2 TO FEED INTO OUTTAKE
+      //ADD A METHOD MAKE THE BOTTOM LIGHTS GREEN TO SHOW THAT ITS READY TO SHOOT
+
+    } else if (avgRPM <= 5000) { //IF THE MOTORS ARE NOT AT THE RIGHT SPEED
+      OUT1.set(1); //MAKE OUTTAKE MOTOR 1 GO TO RIGHT SPEED
+      OUT2.set(1); //MAKE OUTTAKE MOTOR 1 GO TO RIGHT SPEED
+      //ADD A METHOD MAKE THE BOTTOM LIGHTS RED TO SHOW THAT ITS NOT READY TO SHOOT
+      }
+    }
   }
+
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    outtake1.set(0);
-    outtake2.set(0);
+    OUT1.set(0);
+    OUT2.set(0);
+    IN1.set(0);
+    IN2.set(0);
   }
 
   // Returns true when the command should end.
